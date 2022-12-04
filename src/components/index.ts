41d6acd4fe025c9b6c3f5b63c816f6ea98d3b008
Omkar Borude <omkar@ula.app>
2022-12-05 00:24:46 +0530

    FEAT: added popup modal

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,7 +1,6 @@
 export { Header } from "./header";
 export { Logo } from "./logo";
-export { Main } from "./login";
+export { Login } from "./login";
 export { Button } from "./button";
 export { Cards } from "./cards";
-export { Footer } from "./footer";
 export { Container } from "./container";
