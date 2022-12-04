c5368a29880a1407d7eb6e5cc244b5d127eb3ff8
Omkar Borude <omkar@ula.app>
2022-12-04 20:56:43 +0530

    FEAT: added product listing

--- src/components/cards/index.tsx
+++ src/components/cards/index.tsx
@@ -1,21 +1,43 @@
 import React from "react";
 
 import data from "@public/meta.json";
+import {cardProp , product} from "@redux/types"
+export const Cards = (props:cardProp) => {
 
-export const Cards: React.FC = () => {
+const { products } = props;
+
+console.log(products)
   return (
-    <div className="flex-1 container my-8 max-w-screen-lg mx-auto p-5">
-      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6">
-        {(data?.plugins ?? []).map((plugin) => (
-          <div
-            key={plugin.name}
-            className="col-span-1 rounded-md border border-gray-300 p-5"
-          >
-            <h2 className="text-xl font-semibold mb-2">{plugin.name}</h2>
-            <p className="m-0">{plugin.description}</p>
-          </div>
-        ))}
-      </div>
-    </div>
+    <div className="bg-white">
+  <div className="mx-auto max-w-2xl py-16 px-4 sm:py-24 sm:px-6 lg:max-w-7xl lg:px-8">
+    <h2 className="text-2xl font-bold tracking-tight text-gray-900">Customers also purchased</h2>
+    <div className="grid grid-cols-1 gap-y-10 gap-x-6 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 xl:gap-x-8">
+
+{products?.map((item:product) => (
+ <div className="card flex flex-col rounded overflow-hidden shadow-lg">
+ <img className="card__image object-cover h-56 p-4" src={item.image} alt={item.title} />
+ <div className="p-2">
+   <div className="card__title text-center font-bold text-xl break-words">{item.title}</div>
+   <div className="flex justify-center items-center">
+     <div className="card__price text-gray-700 text-xl">${item.price}</div>
+   </div>
+ </div>
+ <div className="flex flex-row justify-between px-4">
+   <button
+     type="button"
+     className=" inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2"
+   >
+     Add to cart +
+   </button>
+
+ </div>
+</div>
+))}
+</div>
+
+  
+  </div>
+</div>
+
   );
 };
