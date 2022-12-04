55b35d29caa65b3c59c8386fab10373d19eee000
Omkar Borude <omkar@ula.app>
2022-12-04 18:56:25 +0530

    FEAT: added home page

---
+++ src/container/home/index.tsx
@@ -0,0 +1,18 @@
+import React, { useEffect } from "react";
+import { useDispatch, useSelector, RootStateOrAny } from "react-redux";
+
+
+
+export const Home: React.FC = () => {
+    const Dispatch = useDispatch();
+
+
+    return (
+        <>
+        <div>
+Home
+        </div>
+        </>
+    )
+
+}
